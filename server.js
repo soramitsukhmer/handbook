e3118f74d068e101fa0dcf9192846d5ca59237ba
Socheat Sok <socheatsok78@gmail.com>
2023-08-09 12:22:04 +0700

    Install zx and update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import pkg from './package.json' assert { type: "json" };
+import { $, echo } from 'zx'
 
-console.group("INFO")
-console.log(`${pkg.name}@${pkg.version} - ${pkg.description}`)
-console.groupEnd()
+try {
+    echo(`\n${pkg.name}@${pkg.version} - ${pkg.description}\n`)
+    await $`vitepress dev`
+} catch {}
